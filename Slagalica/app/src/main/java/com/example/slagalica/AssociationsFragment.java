3c195ca2b21547f85f84baf1bedbd0ba588079e9
maljkovicmarko <markomaljak@gmail.com>
2026-05-09 01:43:53 -0400

    AssociationsFragment.java, fragment_associations.xml, control flow

--- Slagalica/app/src/main/java/com/example/slagalica/AssociationsFragment.java
+++ Slagalica/app/src/main/java/com/example/slagalica/AssociationsFragment.java
@@ -7,27 +7,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ConnectingGameFragment#newInstance} factory method to
+ * Use the {@link AssociationsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ConnectingGameFragment extends Fragment {
+public class AssociationsFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    private Button confirmConnectionButton;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-
-    public ConnectingGameFragment() {
+    public AssociationsFragment() {
         // Required empty public constructor
     }
 
@@ -37,11 +34,11 @@ public class ConnectingGameFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ConnectingGameFragment.
+     * @return A new instance of fragment AssociationsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ConnectingGameFragment newInstance(String param1, String param2) {
-        ConnectingGameFragment fragment = new ConnectingGameFragment();
+    public static AssociationsFragment newInstance(String param1, String param2) {
+        AssociationsFragment fragment = new AssociationsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -62,18 +59,6 @@ public class ConnectingGameFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_connecting_game, container, false);
-
-         confirmConnectionButton = view.findViewById(R.id.confirmConnectionButton);
-
-        confirmConnectionButton.setOnClickListener(v -> {
-            requireActivity()
-                    .getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.fragmentContainer, new AssociationsFragment())
-                    .commit();
-        });
-
-        return view;
+        return inflater.inflate(R.layout.fragment_associations, container, false);
     }
 }
