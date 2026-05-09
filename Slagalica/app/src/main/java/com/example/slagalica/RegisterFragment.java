b6c1cba57cf18c600517bb8da4b5db2f9c4662a8
maljkovicmarko <markomaljak@gmail.com>
2026-05-08 21:37:55 -0400

    register fragment

--- Slagalica/app/src/main/java/com/example/slagalica/RegisterFragment.java
+++ Slagalica/app/src/main/java/com/example/slagalica/RegisterFragment.java
@@ -11,10 +11,10 @@ import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
+ * Use the {@link RegisterFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LoginFragment extends Fragment {
+public class RegisterFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,9 +25,9 @@ public class LoginFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private TextView registerText;
+    private TextView loginText;
 
-    public LoginFragment() {
+    public RegisterFragment() {
         // Required empty public constructor
     }
 
@@ -37,11 +37,11 @@ public class LoginFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
+     * @return A new instance of fragment Register.
      */
     // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
+    public static RegisterFragment newInstance(String param1, String param2) {
+        RegisterFragment fragment = new RegisterFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -62,18 +62,16 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
 
-        registerText = view.findViewById(R.id.registerText);
+        loginText = view.findViewById(R.id.loginText);
 
-        registerText.setOnClickListener(v -> {
+        loginText.setOnClickListener(v -> {
             requireActivity()
                     .getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.fragmentContainer, new RegisterFragment())
-                    .addToBackStack(null)
-                    .commit();
+                    .popBackStack();
         });
+
         return view;
     }
 }
